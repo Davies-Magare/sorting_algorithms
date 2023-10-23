9e6be3088b64740643355c9bcdce29d7a270c004
Davies Magare <davieskamanda@gmail.com>
2023-10-23 09:57:14 +0000

    Task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,63 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers using
+ * the bubble sort algorithm
+ * @array: An array of integers
+ * @size: The size of the array
+ *
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int sorted, i, j, temp;
+
+	sorted = 0;
+	while (!sorted && size > 1)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			if (array[i + 1] < array[i])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+			i++;
+		}
+		j = 0;
+		while (j < size)
+		{
+			if (array[j + 1] < array[j])
+				break;
+			else if (j == size - 2)
+			{
+				sorted = 1;
+				break;
+			}
+			j++;
+		}
+	}
+}
+/**
+ * print_array - prints an array recursively
+ * @array: The array
+ * @size: The size of the array
+ *
+ * Return: Nothing
+ */
+void print_array(int *array, int size)
+{
+	if (size == 1)
+	{
+		print_int(array[0]);
+		_putchar('\n');
+		return;
+	}
+	print_int(array[0]);
+	_putchar(',');
+	_putchar(' ');
+	print_array(&array[1], size - 1);
+}
---
+++ sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stddef.h>
+#include <unistd.h>
+
+void print_int(int n);
+int _putchar(char c);
+void bubble_sort(int *array, size_t size);
+void print_array(int *array, int size);
+
+#endif
